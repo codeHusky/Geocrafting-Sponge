e6297f959643c7add62c20b86ac8b6967353448e
MikaalSky <lokio27productions@gmail.com>
2016-12-20 18:13:22 -0600

    Slight updates

--- src/main/java/pw/codehusky/geocaching/Geocaching.java
+++ src/main/java/pw/codehusky/geocaching/Geocaching.java
@@ -6,14 +6,17 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.item.inventory.ChangeInventoryEvent;
+import org.spongepowered.api.event.item.inventory.ClickInventoryEvent;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.item.inventory.entity.PlayerInventory;
 import org.spongepowered.api.item.inventory.transaction.SlotTransaction;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginContainer;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Plugin(id="geocaching", name = "Geocaching",version = "0.1.0",description = "Does the geocaching.")
@@ -25,14 +28,15 @@ public class Geocaching {
 
     @Inject
     private PluginContainer pC;
-
+    private HashMap<Long,HashMap<UUID,HashMap<String,Boolean>>> geocacheData = new HashMap<>();
     @Listener
     public void onServerStart(GameStartedServerEvent event){
         logger.info("Geocaching v" + pC.getVersion().get() + " starting...");
+        geocacheData.put(0l,new HashMap<>());
     }
 
     @Listener
-    public void onInventoryClick(ChangeInventoryEvent event){
+    public void onInventoryChange(ChangeInventoryEvent event){
         Player cause;
         if(!(event.getCause().all().get(0) instanceof Player)){
             return;
@@ -45,23 +49,34 @@ public class Geocaching {
         }
         Inventory tt = event.getTargetInventory();
         if(tt.getName().get().contains("§a") && tt.getName().get().contains("'s Geocache")){
+            HashMap<UUID,HashMap<String,Boolean>> ourCache = geocacheData.get(0l);
+            if(!ourCache.containsKey(cause.getUniqueId())){
+                ourCache.put(cause.getUniqueId(),new HashMap<>());
+            }
+            HashMap<String,Boolean> personalProperties = ourCache.get(cause.getUniqueId());
             if(event.getTransactions().size() > 0) {
                 SlotTransaction g = event.getTransactions().get(0);
                 Optional<ItemStack> pre = g.getSlot().peek();
-                if(pre.isPresent()){
+                if(pre.isPresent() && false){
                     ItemStack ourStack = pre.get();
                     Inventory t = g.getSlot().parent();
                     if(t.getName().get().contains("§a") && t.getName().get().contains("'s Geocache")){
                         logger.info("Geocache");
+                        if(t.contains(ourStack))
+                            logger.info("geo find");
                     }else{
                         logger.info("Player inventory");
-                        PlayerInventory pI = (PlayerInventory) t;
-                        if(g.getSlot().contains(ourStack))
-                            logger.info("A");
+                        if(t.contains(ourStack))
+                            logger.info("ply find");
                     }
                 }
 
             }
         }
+    }
+    @Listener
+    public void onInventoryClick(ClickInventoryEvent event){
+        logger.info("woah");
+        logger.info(event.getTransactions().get(0).getSlot().parent().getName().get());
     }
 }
