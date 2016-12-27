3bcedd65cb2ae8696df6f8f04afc23cc6b4b3f69
MikaalSky <lokio27productions@gmail.com>
2016-12-26 21:17:53 -0600

    Getting closer, but not quite there.

--- src/main/java/pw/codehusky/geocaching/GeoWithdraw.java
+++ src/main/java/pw/codehusky/geocaching/GeoWithdraw.java
@@ -16,48 +16,51 @@ import java.util.Optional;
  */
 public class GeoWithdraw {
     public static Inventory create(ItemStack stack, String cacheID, Geocaching plugin){
-        Inventory redeem = Inventory.builder()
-                .of(InventoryArchetypes.CHEST)
-                .property(InventoryTitle.PROPERTY_NAME, InventoryTitle.of(Text.of("Take Item")))
-                .listener(ClickInventoryEvent.class, evt2 -> {
-                    System.out.println("uh");
-                    if(evt2.getTransactions().size() == 0)
-                        return;
-                    Inventory clickd = evt2.getTransactions().get(0).getSlot().parent();
-                    String g = clickd.getName().get();
-                    if(g.equals("Take Item")){
-                        Player plr = (Player)evt2.getCause().root();
-                        ItemStackSnapshot sn = evt2.getCursorTransaction().getFinal();
-                        plr.getInventory().offer(sn.createStack());
-                        plr.closeInventory(plugin.genericCause);
-                        try {
-                            Optional<TileEntity> tt = plugin.getGeocacheLocation(cacheID).getTileEntity();
-                            if(tt.isPresent()){
-                                TileEntity next = tt.get();
-                                if(next instanceof TileEntityCarrier){
-                                    Inventory updatable = ((TileEntityCarrier) next).getInventory();
-                                    for(Inventory ps: clickd.slots()){
-                                        Slot s = (Slot) ps;
-                                        if(s.peek().isPresent()){
-                                            if(s.peek().get() != sn.createStack())
-                                                updatable.offer(s.peek().get());
-                                        }
-                                    }
-                                }
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        //plugin.updateGeocacheContents(cacheID,clickd);
-                    }
-                }).build(Sponge.getPluginManager().getPlugin("geocaching").get());
+
         try {
             Optional<TileEntity> tt = plugin.getGeocacheLocation(cacheID).getTileEntity();
             if(tt.isPresent()) {
                 TileEntity next = tt.get();
                 if (next instanceof TileEntityCarrier) {
                     Inventory updatable = ((TileEntityCarrier) next).getInventory();
-                    for (Inventory e : updatable.slots()) {
+                    Inventory redeem = Inventory.builder()
+                            .of(InventoryArchetypes.CHEST)
+                            .property(InventoryTitle.PROPERTY_NAME, InventoryTitle.of(Text.of("Take Item")))
+                            .withCarrier((TileEntityCarrier) next)
+                            .listener(ClickInventoryEvent.class, evt2 -> {
+                                System.out.println("uh");
+                                if(evt2.getTransactions().size() == 0)
+                                    return;
+                                Inventory clickd = evt2.getTransactions().get(0).getSlot().parent();
+                                String g = clickd.getName().get();
+                                if(g.equals("Take Item")){
+                                    Player plr = (Player)evt2.getCause().root();
+                                    ItemStackSnapshot sn = evt2.getCursorTransaction().getFinal();
+                                    plr.getInventory().offer(sn.createStack());
+                                    plr.closeInventory(plugin.genericCause);
+                                    try {
+                                        Optional<TileEntity> tt2 = plugin.getGeocacheLocation(cacheID).getTileEntity();
+                                        if(tt2.isPresent()){
+                                            TileEntity next2 = tt2.get();
+                                            if(next2 instanceof TileEntityCarrier){
+                                                Inventory updatable2 = ((TileEntityCarrier) next2).getInventory();
+                                                for(Inventory ps: clickd.slots()){
+                                                    Slot s = (Slot) ps;
+                                                    if(s.peek().isPresent()){
+                                                        if(s.peek().get() != sn.createStack())
+                                                            updatable2.offer(s.peek().get());
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    //plugin.updateGeocacheContents(cacheID,clickd);
+                                }
+                            }).build(Sponge.getPluginManager().getPlugin("geocaching").get());
+                    Iterable<Slot> t = updatable.slots();
+                    for (Inventory e : t) {
                         Slot s = (Slot) e;
                         Optional<ItemStack> stackmaybe = s.peek();
                         if (stackmaybe.isPresent()) {
@@ -66,6 +69,7 @@ public class GeoWithdraw {
                         }
                     }
                     updatable.offer(stack);
+                    return redeem;
                 }else{
                     System.out.println("Excuse me?");
                 }
@@ -75,6 +79,6 @@ public class GeoWithdraw {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return redeem;
+        return null;
     }
 }
